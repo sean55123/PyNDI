653c206680df453e964662c6104b5beb83eadff4
Hsuan Han Chiu <112639406+sean55123@users.noreply.github.com>
2024-12-26 22:14:46 -0500

    Update readme

--- setup.py
+++ setup.py
@@ -16,5 +16,6 @@ setup(
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.6",
-    install_requires=["numpy"],  
+    install_requires=["numpy",
+                      "win32"],  
 )
